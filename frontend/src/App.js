c2f032f9550200b74ab4a26de52a8eb526033cbc
prijay maharjan <prijaymahrjan@gmail.com>
2020-07-25 23:04:05 +0545

    Working FrontEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Home Page</h2>
+    </div>
+  );
+};
 export default App;
